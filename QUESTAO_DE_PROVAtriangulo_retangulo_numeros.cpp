894ef091aff992063228c0d56f5c06b9ce88c461
PedroRockembach <rockpedro11@gmail.com>
2025-05-14 10:00:44 -0300

    TRIANGULO DE NUMEROS

---
+++ QUESTAO_DE_PROVAtriangulo_retangulo_numeros.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int altura;
+	int contador = 0;
+	
+	printf("Digite a altura do triangulo: ");
+	scanf("%d",&altura);
+	
+	for (int l = 0; l<=altura; l++)
+	{
+		for (int c = 0;c <= l; c++)	
+		{
+			contador += 1;
+			printf("%d ",contador);
+		}
+		printf("\n");
+	}
+	
+	
+	
+	
+}
