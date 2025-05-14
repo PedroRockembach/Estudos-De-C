7f5423574a3b0f7bd918dc27328cad2b5488500c
Pedro Rockembach <rockpedro11@gmail.com>
2025-05-14 16:15:32 -0300

    loja de eletronicos V2 FINALIZADO.

--- Q_PROVA_loja de eletronicos.cpp
+++ Q_PROVA_loja de eletronicos.cpp
@@ -10,17 +10,23 @@ int main(){
 	int saida = 1;
 	float preco_final;
 	
-printf("------TABELA DE COMPONENTES------");
-printf("\n1 - Resistor = R$0.10\n2 - Capacitor = R$0.20\n3 - Indutor = R$0.30\n4 - Diodo = R$0.40\n5 - LED = R$0.50\n6 - Transistor = R$0.60");
 
 	while(1)
 	{
-
+		
+	system("cls");
+	
+	printf("------TABELA DE COMPONENTES------");
+	printf("\n1 - Resistor = R$0.10\n2 - Capacitor = R$0.20\n3 - Indutor = R$0.30\n4 - Diodo = R$0.40\n5 - LED = R$0.50\n6 - Transistor = R$0.60");
+	
 	printf("\n---------------------------------\nO usuario deseja comprar algo?\n[1] Sim\n[2] Nao\n---> ");
 	scanf("%d",&saida);	
 	
 	if (saida == 2)
-	{
+	
+	{	
+		printf("preco final = %f\n",soma);
+		printf("O PROGRAMA SERA ENCERRADO"); 	
 		break;
 	}	
 	
@@ -33,25 +39,97 @@ printf("\n1 - Resistor = R$0.10\n2 - Capacitor = R$0.20\n3 - Indutor = R$0.30\n4
 		printf("Quantos deste componente voce deseja? ");	
 		scanf("%f",&quant);
 		
-		if (comp == 1)
+		
+		
+		if (comp == 1) //RESISTOR
+		{
+		
+			if (quant<10)
+			
+			{
+				soma += quant*0.10;
+			}
+			
+			else 
+			{
+				
+				soma += (quant*0.10);
+					
+				if (soma > 15.00)
+				
+					{
+					soma += (quant*0.10)-(quant*0.10)*0.10;	
+					}	
+			}
+		}		
+		
+		
+		
+		if (comp == 2) // CAPACITOR
 		{
 		
 			if (quant<15)
 			
 			{
-			soma += quant*0.10;
+				soma += quant*0.20;
 			}
 			
 			else
 			{
-			soma += (quant*0.10)-(quant*0.10)*0.10;
+				soma += (quant*0.20);
+				
+				if (soma>16.00)
+				{
+				soma -= soma*0.05;
+				}
+				
+			}
+		}
+
+
+
+		if (comp == 3) // INDUTOR
+		{
+		
+			if (quant<20)
+			
+			{
+				soma += quant*0.30;
 			}
 			
-		preco_final = soma;
-		}
+			else
+			{
+				soma += (quant*0.30);
+				
+				if (soma>17.00)
+				{
+				soma += (quant*0.30)-(quant*0.30)*0.04;					
+				soma -= soma*0.08;
+				}
+				
+			}
+		}	
 		
+			
+		if (comp == 4) // DIODO
+		{
 		
-	}	
+				soma += quant*0.40;
+		}		
+		if (comp == 5) // lED
+		{
+		
+				soma += quant*0.50;
+		}		
+		if (comp == 6) // TRANSISTOR
+		{
+		
+				soma += quant*0.60;
+		}		
+	
+	
+	}
+		
 		
 		
 		
