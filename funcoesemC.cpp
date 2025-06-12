abb0dd4b8c0ca163294fa996b143e85b38d466fd
PedroRockembach <rockpedro11@gmail.com>
2025-06-12 08:18:50 -0300

    testefunção

---
+++ funcoesemC.cpp
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void printa_string(const char *mensagem)
+{	
+	printf("%s\n",mensagem);	
+}
+
+int somar(int a, int b)
+{
+	return a + b;
+}
+
+void strip(const char *mensagem)
+{
+	#include <string.h>
+	tamanho = strlen(mensagem)
+	
+	for(int t = 0;t <= tamanho;t++)
+	{
+		
+		
+	}
+	
+	
+}
+
+int main()
+{
+	int resultado = somar(9,8);
+	print("ola mundo");
+	
+	printf("%d",resultado);
+	
+	
+	
+	
+}
