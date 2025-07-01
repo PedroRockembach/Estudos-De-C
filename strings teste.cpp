f6985091ba565b54ea96b28b7c08122d3b0343cb
PedroRockembach <rockpedro11@gmail.com>
2025-07-01 13:40:37 -0300

    teste strings menu matrizes

---
+++ strings teste.cpp
@@ -0,0 +1,20 @@
+//Strings
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+	char frase[500];
+	
+	printf("digite uma frase: ");
+	scanf("%30[^\n]",frase);
+	
+	printf("%s",frase);
+	
+	
+	
+	
+	
+}
