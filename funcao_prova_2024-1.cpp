431170833df264750758ee2b93804fdf654143e1
PedroRockembach <rockpedro11@gmail.com>
2025-07-17 13:18:57 -0300

    PROVA RS

---
+++ funcao_prova_2024-1.cpp
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+ 
+void criptografia(char palavra[],int vetor_mudanca[])
+{
+	int tamanho = strlen(palavra);
+	
+	// A -  Se o valor da posicao for 1 e esta posicao for minuscula, muda para maiuscula
+	
+		for(int percorre = 0; percorre < tamanho; percorre++)
+		{
+			if (vetor_mudanca[percorre] == 1)
+			{
+				if (palavra[percorre] >= 'a' && palavra[percorre] <= 'z')
+				{ 
+				// tomar cuidado com a posicao do vetor e o igual na comparação
+					palavra[percorre] = palavra[percorre] - 32;
+				
+				}
+				
+				else
+				{
+					if (palavra[percorre] >= 'A' && palavra[percorre] <= 'Z')
+					{ 
+					// tomar cuidado com a posicao do vetor e o igual na comparação
+						palavra[percorre] = palavra[percorre] + 32;
+					}
+				}
+										
+			}
+
+		}
+	//pinto
+	// B - 
+	
+		for(int percorre = 0; percorre < tamanho; percorre++)
+		{
+			if (vetor_mudanca[percorre] == 2)
+			{
+				palavra[percorre] = palavra[percorre] / 2;
+				
+			}	
+		}		
+	
+	// C -
+	
+	//Saida
+		printf("Palavra Transformada: %s",palavra);	
+}
+
+int main()
+{
+	printf("BEM VINDO A CRIPTOGRAFIA\n");
+	printf("INSTRUCOES:\n-Ao digitar 1 a posicao da chave, a letra vai inverter sua capitalizacao(a = A or A = a)\n-Ao digitar 2, o codigo ascci do valor naquela posicao vai ser dividido por 2.\n\n");
+	
+	char palavra[300];	
+	
+	printf("digite uma palavra: ");
+	scanf(" %s",palavra);
+	
+	printf("Palavra antes: %s\n\n",palavra);
+	
+	int tamanho = strlen(palavra);
+	int mudanca[tamanho];
+	
+		for (int c = 0;c < tamanho;c++)
+		{
+			printf("Digite o valor da pos[%d] da chave: ",c);
+			scanf("%d",&mudanca[c]);
+			
+		}
+		
+		printf("\n");
+		printf("Essa eh a chave de criptografia: ");
+		
+		for (int mostra = 0;mostra < tamanho;mostra++)
+		{
+			printf("%d",mudanca[mostra]);
+		}
+			
+		printf("\n");
+	
+	
+	criptografia(palavra,mudanca);
+
+	
+}
